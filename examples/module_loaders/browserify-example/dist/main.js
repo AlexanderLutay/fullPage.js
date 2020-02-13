b8edfe6513ccad29b7dc8bca27b732cb252196cc
Alex K Lutay <ravendestiny@gmail.com>
2020-02-13 15:47:38 +0700

    Fixed NodeList for ssr

--- examples/module_loaders/browserify-example/dist/main.js
+++ examples/module_loaders/browserify-example/dist/main.js
@@ -3790,7 +3790,7 @@
     * forEach polyfill for IE
     * https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach#Browser_Compatibility
     */
-    if (window.NodeList && !NodeList.prototype.forEach) {
+    if (window.NodeList && !window.NodeList.prototype.forEach) {
         NodeList.prototype.forEach = function (callback, thisArg) {
             thisArg = thisArg || window;
             for (var i = 0; i < this.length; i++) {
